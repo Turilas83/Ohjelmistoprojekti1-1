f96e4261f52944b6fc29ffc466cba405f625bbdd
Mark Laatikainen <marklaatikainen@users.noreply.github.com>
2017-11-01 11:03:53 +0200

    Create SaveAnswersDAO.java

---
+++ ohjelmistoprojekti1/src/main/java/ohjelmistoprojekti1/dao/SaveAnswersDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+public class SaveAnswersDAO {
+	
+	// tallenna tekstivastaus
+	public void saveTextAnswer(String str) {
+		
+	}
+	// tallenna skaalavastaus
+	public void saveScaleAnswer(String str) {
+		
+	}
+	// tallenna multivalintavastaus
+	public void saveMultipleChoiceAnswer(String str) {
+		
+	}
+	// tallenna checkboxvastaus
+	public void saveCheckBoxAnswer(String str) {
+		
+	}
+	
+	
+}
